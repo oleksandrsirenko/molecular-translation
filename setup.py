34aa48abccebcc6cc492415354923833e98ca5cc
Oleksandr Sirenko <oleksa.sirenko@gmail.com>
2021-03-18 00:04:17 +0200

    define basic project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Develop an algorithm to predict the corresponding International Chemical Identifier (InChI) text string from old chemical images.',
+    author='Oleksandr Sirenko',
+    license='MIT',
+)
